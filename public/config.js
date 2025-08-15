b5ebb14e162b4a761988cbaeeedacf34cae17561
yusuf-bolat <yusuf.k.bolat@gmail.com>
2025-08-16 01:12:27 +0900

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 // Configuration for Sukejuru Calendar
 // This file uses placeholder that gets replaced during deployment
 
-window.OPENAI_API_KEY = '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';
+window.OPENAI_API_KEY = 'OPENAI_API_KEY';
 
 // This placeholder will be replaced during Vercel deployment
 // with the actual environment variable value
